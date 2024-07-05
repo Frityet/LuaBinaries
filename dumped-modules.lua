290f6190b391500d3a1d7640f520292ca40b032a
Amrit Bhogal <ambhogal01@gmail.com>
2024-07-04 22:34:54 -0700

    should be committed

---
+++ dumped-modules.lua
@@ -0,0 +1,21 @@
+return {
+    ["zlib"] = "/Users/frityet/Documents/LuaBinaries/lua_modules/lib/lua/5.4/zlib.so";
+    ["builders.puc.windows"] = "/Users/frityet/Documents/LuaBinaries/lua_modules/share/lua/5.4/builders/puc/windows.lua";
+    ["zip"] = "/Users/frityet/Documents/LuaBinaries/lua_modules/lib/lua/5.4/zip.so";
+    ["builders.jit.download"] = "./builders/jit/download.lua";
+    ["builders.jit.posix"] = "./builders/jit/posix.lua";
+    ["builders.jit.windows"] = "./builders/jit/windows.lua";
+    ["utilities.Path"] = "./utilities/Path.lua";
+    ["builders.puc"] = "/Users/frityet/Documents/LuaBinaries/lua_modules/share/lua/5.4/builders/puc/init.lua";
+    ["builders.puc.download"] = "./builders/puc/download.lua";
+    ["$!main!$"] = "binary-creator.lua";
+    ["builders.jit"] = "./builders/jit/init.lua";
+    ["utilities"] = "/Users/frityet/Documents/LuaBinaries/lua_modules/share/lua/5.4/utilities/init.lua";
+    ["lfs"] = "/Users/frityet/Documents/LuaBinaries/lua_modules/lib/lua/5.4/lfs.so";
+    ["builders.puc.posix"] = "/Users/frityet/Documents/LuaBinaries/lua_modules/share/lua/5.4/builders/puc/posix.lua";
+    ["builders"] = "/Users/frityet/Documents/LuaBinaries/lua_modules/share/lua/5.4/builders/init.lua";
+    ["utilities.tar"] = "./utilities/tar.lua";
+    ["easyhttp"] = "/Users/frityet/Documents/LuaBinaries/lua_modules/lib/lua/5.4/easyhttp.so";
+    ["argparse"] = "/Users/frityet/Documents/LuaBinaries/lua_modules/share/lua/5.4/argparse.lua";
+    ["sysdetect"] = "/Users/frityet/Documents/LuaBinaries/lua_modules/share/lua/5.4/sysdetect.lua";
+}
